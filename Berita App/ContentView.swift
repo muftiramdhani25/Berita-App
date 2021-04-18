f91f44dfc46ab14feef399ce3922c54b97f9ff70
Mufti Ramdhani <muftiramdhani@gmail.com>
2021-04-18 17:47:09 +0700

    Initial Commit

---
+++ Berita App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Berita App
+//
+//  Created by Mufti Ramdhani on 18/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
